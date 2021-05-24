abea1944128f37c37493f6733b799f5742fa0371
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-05-24 17:25:14 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextExercise5
+{
+    class Program
+    {
+        //Write a program and ask the user to enter an English word.
+        //Count the number of vowels (a, e, o, u, i) in the word.
+        //So, if the user enters "inadequate", the program should display 6 on the console.
+        static void Main(string[] args)
+        {
+            Console.Write("Type an English word into the console: ");
+            var input = Console.ReadLine().ToLower();
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'i', 'o', 'u', });
+            var vowelsCount = 0;
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelsCount++;
+            }
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
